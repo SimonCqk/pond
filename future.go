4a20cc56db8a6a001ef6a562a48101b4918fe2be
Simoncqk <cqk0100@gmail.com>
2018-12-04 22:35:21 +0800

    finish future implementation.

--- future.go
+++ future.go
@@ -3,7 +3,7 @@ package pond
 // Future associate with a Task instance and can be used to capture
 // return value of task.
 type Future interface {
-	// Value return the value captured by Future.
+	// Value synchronously return the value captured by Future.
 	Value() (interface{}, error)
 
 	// Then allows multiple functions chained together, the semantic is
@@ -12,14 +12,55 @@ type Future interface {
 	Then(next func(interface{}) (interface{}, error)) Future
 
 	// OnSuccess provide the callback when future executed successfully.
-	OnSuccess(f func(interface{})) Future
+	// If task done with error, it is a no-op.
+	OnSuccess(f func(interface{}))
 
 	// OnFailure provide the callback when future done with some error.
-	OnFailure(f func(err error)) Future
+	// If task done with success, it is a no-op.
+	OnFailure(f func(error))
 }
 
 type pondFuture struct {
 	value interface{}
 	err   error
 	done  chan struct{}
+}
+
+func newPondFuture(next func() (interface{}, error)) *pondFuture {
+	f := pondFuture{done: make(chan struct{})}
+	go func() {
+		f.value, f.err = next()
+		f.done <- struct{}{}
+	}()
+	return &f
+}
+
+func (pf *pondFuture) Value() (interface{}, error) {
+	// block for done
+	<-pf.done
+	return pf.value, pf.err
+}
+
+func (pf *pondFuture) Then(next func(interface{}) (interface{}, error)) Future {
+	return newPondFuture(func() (interface{}, error) {
+		val, err := pf.Value()
+		if err != nil {
+			return val, err
+		}
+		return next(val)
+	})
+}
+
+func (pf *pondFuture) OnSuccess(f func(interface{})) {
+	val, err := pf.Value()
+	if err == nil {
+		f(val)
+	}
+}
+
+func (pf *pondFuture) OnFailure(f func(err error)) {
+	_, err := pf.Value()
+	if err != nil {
+		f(err)
+	}
 }
