fced608cafd555ffa67afd251ec52f093286c22e
Simoncqk <cqk0100@gmail.com>
2019-02-17 16:02:55 +0800

    add travis CI config file.

--- pond/pool_test.go
+++ pond/pool_test.go
@@ -13,7 +13,6 @@ func foo() (interface{}, error) {
 
 func TestBasicPoolSubmit(t *testing.T) {
 	fmt.Println(t.Name())
-	fmt.Println(t.Name())
 	pool := NewPool()
 	future, _ := pool.Submit(foo)
 	_, _ = future.Value()
